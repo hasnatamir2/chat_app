62556b95bdc3ee50eca6ec66928bf4193d378a84
IDEALAB-BETA1\hasna <hasnatamir2@gmail.com>
2020-11-18 08:15:09 +0500

    client/server connection wired, message passing to all connected user

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket = io()
+
+socket.on('connect', () => {
+    console.log('connected to server')
+    
+})
+
+socket.on('disconnect', () => {
+    console.log('disconnected from server')
+})
+
+socket.on('newMessage', (message) => {
+    console.log('newMessage', message)
+})
--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 const publicPath = path.join(__dirname + '/../public')
 var port = process.env.PORT || 3000
 var app  = express()
 
+var server = http.createServer(app)
+var io = socketIO(server)
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('connection made')
+    
+    socket.on('createMessage', (message) => {
+        console.log('message', message)
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        })
+    })
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from client')
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is up at Port ${port}`)
 })
